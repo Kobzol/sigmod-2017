78dff3a50e1db4e2311b5b1f86eb1a56c6f32419
Jakub Beránek <berykubik@gmail.com>
2017-03-03 20:03:36 +0100

    ENH: modify state initial size for X-Large

--- src/settings.h
+++ src/settings.h
@@ -14,7 +14,7 @@ using DictHash = unsigned int;
 #define THREAD_COUNT (8)
 #define JOB_SPLIT_SIZE (5000UL)
 #define WORDMAP_HASH_SIZE (2 << 25)
-#define NFA_STATES_INITIAL_SIZE (2 << 22)
+#define NFA_STATES_INITIAL_SIZE (2 << 26)
 #define MAX_LINEAR_MAP_SIZE (50)
 #define DICTIONARY_HASH_MAP_SIZE (2 << 24)  // must be a power of two
 #define LINEAR_MAP_SIZE (1024 * 1024 * 10)
